c65f90307b2bdb668e3829ba1b75e0efba0fa13e
Irving Guerra <guerravargasirving@gmail.com>
2020-06-10 19:20:28 -0500

    Multicast

---
+++ src/downloadsrmi/database.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package downloadsrmi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author irvingguerra
+ */
+public class database {
+   
+    private List<serverData> ServersList = new ArrayList<>();
+
+    public List<serverData> getServersList() {
+        return ServersList;
+    }
+    
+    public void addServer(serverData e) {
+        ServersList.add(e);
+    }
+
+    public void setServersList(List<serverData> ServersList) {
+        this.ServersList = ServersList;
+    }
+
+}
